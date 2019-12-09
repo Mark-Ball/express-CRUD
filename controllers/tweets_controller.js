373a0aebc52b5f9bbbe31ab3b39288954baaae86
Mark-Ball <markball111@gmail.com>
2019-12-09 17:59:27 +1100

    implemented creation of new tweets

--- controllers/tweets_controller.js
+++ controllers/tweets_controller.js
@@ -1,9 +1,24 @@
-const tweets = ["I'm a tweet"];
+let tweets = [
+    { tweet: "I'm a tweet", name: "Mark" }
+];
 
 const index = (req, res) => {
     res.render('tweets/index', { tweets });
 }
 
+const tweetForm = (req, res) => {
+    res.render('tweets/form');
+}
+
+const create = (req, res) => {
+    console.log(req.body);
+    let { name, tweet } = req.body;
+    tweets.push({name, tweet});
+    res.redirect('/tweets');
+}
+
 module.exports = {
-    index
+    index,
+    tweetForm,
+    create
 }
--- routes.js
+++ routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const TweetsController = require("./controllers/tweets_controller");
 
 router.get("/tweets", TweetsController.index);
+router.get("/tweets/new", TweetsController.tweetForm);
+router.post("/tweets", TweetsController.create);
 
 module.exports = router;
